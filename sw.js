0d2ec498ff545634912e340478c62299e5b97bf1
lcossewold-sketch <l.c.ossewold@gmail.com>
2026-07-28 08:10:30 +0200

    Add Service Worker for caching assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'life-rpg-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Installeren van de Service Worker en bestanden opslaan
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Bestanden ophalen uit cache als er geen internet is
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
